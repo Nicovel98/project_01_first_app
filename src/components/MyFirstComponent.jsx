386b7dad663743f951d8754896215ba576ff9e0d
Nicovel <nicovel98@gmail.com>
2024-06-27 08:02:42 -0500

    First Component

---
+++ src/components/MyFirstComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function MyFirstComponent() {
+    return (
+        <>
+            <div>MyFirstComponent</div>
+            <div>Hello World I'm Nicolás</div>
+            <div>27 de Junio de 2024</div>
+        </>
+
+    )
+}
+
+export default MyFirstComponent
